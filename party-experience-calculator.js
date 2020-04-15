08382869d15853b9d598747c91bdab2fe8c3e6a9
Kevin Quintino <42918428+kevinnicolasbq@users.noreply.github.com>
2020-04-15 19:50:09 -0300

    Add files via upload

---
+++ party-experience-calculator.js
@@ -0,0 +1,9 @@
+function calculatePartyExperience(characterLevel) {
+    if (characterLevel < 2) { //Verifies if the input number is > 1
+        console.log("The character level must be greater than 1!");
+    } else {
+        let minimumLevel = Math.round(characterLevel / 3 * 2); //Calculates the minimum level with the formula
+        let maximumLevel = Math.round(characterLevel / 2 * 3); //Calculates the maximum level with the formula
+        console.log("Minimum level: " + minimumLevel + " | Maximum level: " + maximumLevel); //Outputs the results
+    }
+}
